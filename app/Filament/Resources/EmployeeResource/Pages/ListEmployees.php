47f02b9a70687a2d7b432488e7eb0105acd62a7d
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2024-07-01 12:49:37 +0700

    create tab record

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use App\Filament\Resources\EmployeeResource;
+use App\Models\Employee;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListEmployees extends ListRecords
 {
@@ -14,6 +17,26 @@ class ListEmployees extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All')
+                ->badge(Employee::count()),
+            'active' => Tab::make('Active')
+                ->badge(Employee::where('status', 'active')->count())
+                ->badgeColor('success')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'active')),
+            'inactive' => Tab::make('In Active')
+                ->badge(Employee::where('status', 'inactive')->count())
+                ->badgeColor('danger')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'inactive')),
+            'on_leave' => Tab::make('On Leave')
+                ->badge(Employee::where('status', 'on_leave')->count())
+                ->badgeColor('warning')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'on_leave')),
         ];
     }
 }
